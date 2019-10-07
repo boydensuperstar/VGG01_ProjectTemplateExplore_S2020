8a6ca0819aceb5ea671b2575c97b809203dd6ac3
boydensuperstar <jeremybwarner@gmail.com>
2019-10-07 11:16:51 -0700

    added collectable object

---
+++ VGG01_ProjectTemplateExplore/Assets/Scripts/CollectableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableObject : MonoBehaviour
+{
+    public int RotateSpeed;
+    public AudioSource CollectSound;
+    public GameObject ThisObject;
+
+
+    void Update()
+    {
+        RotateSpeed = 2;
+        transform.Rotate(0, RotateSpeed, 0, Space.World);
+    }
+
+
+    void OnTriggerEnter()
+    {
+        CollectSound.Play();
+        ThisObject.SetActive(false);
+    }
+}
