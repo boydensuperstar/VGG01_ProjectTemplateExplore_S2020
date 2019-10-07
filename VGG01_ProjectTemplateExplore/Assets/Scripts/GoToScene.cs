655ba4f7de2b35974a0b2c664a4e4a04bec02dcd
boydensuperstar <jeremybwarner@gmail.com>
2019-10-07 07:43:06 -0700

    Added go to scene example

---
+++ VGG01_ProjectTemplateExplore/Assets/Scripts/GoToScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// since we are using scene management, we need to add the UnityEngine Scenemanagement class
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour
+{
+    //This is a public variable allowing you to type the name of the scene in the component instead of putting it in the script
+    public string SceneName;
+
+    // Once the character touches the collider, the character will load the new scene
+    private void OnTriggerEnter(Collider other)
+    {
+        // In the console, it will appear that the character hit the trigger saying that the character entered the public variable string name
+        Debug.Log("Entered " + SceneName);
+        // Which scene to load based upon the public variable above
+        SceneManager.LoadScene(SceneName);
+    }
+}
