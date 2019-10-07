c313b30c3e3ed7eb7fb3c2f9be391b464e353772
boydensuperstar <jeremybwarner@gmail.com>
2019-10-07 08:25:07 -0700

    added menu UI scripts

---
+++ VGG01_ProjectTemplateExplore/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// since we are changing scenes, we need scene management
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    // on hitting play game it will load the next scene in the build.  If the lobby is not the next scene, it will load whatever scene is after the main menu
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // on hitting quit, it will quit the game
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
---
+++ VGG01_ProjectTemplateExplore/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// since this is using audio, we will need to call up the unity engine's audio class
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    // public variable asking for the audio mixer
+    public AudioMixer audioMixer;
+
+    // a method called on where to set the volume based on a float which will be set with a slider
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
