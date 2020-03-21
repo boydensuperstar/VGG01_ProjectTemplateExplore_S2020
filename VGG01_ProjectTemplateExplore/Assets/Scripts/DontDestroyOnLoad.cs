d87e1ab21035fb220eabcaaf2c1d745746ba4750
boydensuperstar <jwarner6@csub.edu>
2020-03-21 15:32:39 -0700

    updated project

---
+++ VGG01_ProjectTemplateExplore/Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    private static int SameScene;
+  
+    public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Don't Destroy on Load " + gameObject.name);
+    }
+
+    public void Start()
+    {
+
+        SameScene = SceneManager.GetActiveScene().buildIndex;
+
+        int ThisScene = SceneyMana.ThisScene;
+
+        Debug.Log("ThisScene is " + ThisScene);
+        Debug.Log("SameScene is " + SameScene);
+
+        if (ThisScene == SameScene)
+        {
+            SceneManager.LoadScene("MainMenu");
+
+        }
+        else
+        {
+            SceneManager.LoadScene(ThisScene);
+        }
+    }
+
+}
+
